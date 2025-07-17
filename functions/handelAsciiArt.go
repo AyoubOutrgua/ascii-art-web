1ed9458b19158c25be4f5877e851e16a57a56125
ayouboutrgua <outrgua1949@gmail.com>
2025-07-17 13:18:33 +0100

    ascii art web

--- functions/handelAsciiArt.go
+++ functions/handelAsciiArt.go
@@ -1,26 +1,19 @@
 package functions
 
-import (
-	"fmt"
-)
-
-func HandelAsciiArt(inputText, banner string) string {
+func HandelAsciiArt(inputText, banner string) (string, bool) {
 	// check range of printable characters
 	if !CheckRange(inputText) {
-		fmt.Println("error, string is not valid ")
-		return ""
+		return "", true
 	}
 
 	// check the validity of the banner
 	if !CheckBanner(banner) {
-		fmt.Println(banner)
-		fmt.Println("error, banner is not valid ")
-		return ""
+		return "", true
 	}
 
 	// split input text to slice of string
 	wordsSlice := SplitInput(inputText)
 
 	// calling functions to handle the input
-	return AppendArt(wordsSlice, AsciiArtTable(banner))
+	return AppendArt(wordsSlice, AsciiArtTable(banner)), false
 }
--- main.go
+++ main.go
@@ -17,24 +17,50 @@ func main() {
 }
 
 func handlerMainFunc(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "404 Not Found!", http.StatusNotFound)
+		return
+	}
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
+		http.Error(w, "500 Internal Server Error!", http.StatusInternalServerError)
 		return
 	}
 	tmpl.Execute(w, nil)
 }
 
 func handlerArtFunc(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "405 Method Not Allowed!", http.StatusMethodNotAllowed)
+		return
+	}
 	tmp, err := template.ParseFiles("templates/index.html")
 	if err != nil {
+		http.Error(w, "500 Internal Server Error!", http.StatusInternalServerError)
+		return
+	}
+	errorForm := r.ParseForm()
+	if errorForm != nil {
+		http.Error(w, "400 Bad Request!", http.StatusBadRequest)
 		return
 	}
 
-	data := map[string]string{
-		"banner": r.FormValue("banner"),
-		"text":   r.FormValue("text"),
+	text, checkText := r.PostForm["text"]
+	if !checkText {
+		http.Error(w, "400 Bad Request!", http.StatusBadRequest)
+		return
 	}
-	
-	data["result"] = functions.HandelAsciiArt(data["text"], data["banner"])
-	tmp.Execute(w, data)
+
+	banner, CheckBanner := r.PostForm["banner"]
+	if !CheckBanner {
+		http.Error(w, "400 Bad Request!", http.StatusBadRequest)
+		return
+	}
+
+	result, checkError := functions.HandelAsciiArt(text[0], banner[0])
+	if checkError {
+		http.Error(w, "400 Bad Request!", http.StatusBadRequest)
+		return
+	}
+	tmp.Execute(w, result)
 }
