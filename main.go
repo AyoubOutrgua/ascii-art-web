9c2502aaa9d9239d6fcb6a6498ff6b476611da31
Ayoub Outrgua <74433630+Ayoub-Outrgua@users.noreply.github.com>
2025-07-19 16:47:54 +0000

    ascii art web

--- main.go
+++ main.go
@@ -23,19 +23,16 @@ func main() {
 
 func handlerMainFunc(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		// http.Error(w, "404 Not Found!", http.StatusNotFound)
 		handleError(w, "Not Found!", http.StatusNotFound)
 		return
 	}
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		// http.Error(w, "500 Internal Server Error!", http.StatusInternalServerError)
 		handleError(w, "Internal Server Error!", http.StatusInternalServerError)
 		return
 	}
 	execError := tmpl.Execute(w, nil)
 	if execError != nil {
-		// http.Error(w, "500 Internal Server Error!", http.StatusInternalServerError)
 		handleError(w, "Internal Server Error!", http.StatusInternalServerError)
 		return
 	}
@@ -43,51 +40,43 @@ func handlerMainFunc(w http.ResponseWriter, r *http.Request) {
 
 func handlerArtFunc(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		// http.Error(w, "405 Method Not Allowed!", http.StatusMethodNotAllowed)
 		handleError(w, "Method Not Allowed!", http.StatusMethodNotAllowed)
 		return
 	}
 	tmp, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		// http.Error(w, "500 Internal Server Error!", http.StatusInternalServerError)
 		handleError(w, "Internal Server Error!", http.StatusInternalServerError)
 		return
 	}
 	errorForm := r.ParseForm()
 	if errorForm != nil {
-		// http.Error(w, "400 Bad Request!", http.StatusBadRequest)
 		handleError(w, "Bad Request!", http.StatusBadRequest)
 		return
 	}
 
 	text, checkText := r.PostForm["text"]
 	if !checkText {
-		// http.Error(w, "400 Bad Request!", http.StatusBadRequest)
 		handleError(w, "Bad Request!", http.StatusBadRequest)
 		return
 	}
 
 	banner, CheckBanner := r.PostForm["banner"]
 	if !CheckBanner {
-		// http.Error(w, "400 Bad Request!", http.StatusBadRequest)
 		handleError(w, "Bad Request!", http.StatusBadRequest)
 		return
 	}
 	cleanText := strings.ReplaceAll(text[0], "\r", "")
 	if len(cleanText) > 1000 || len(cleanText) == 0 {
-		// http.Error(w, "400 Bad Request!", http.StatusBadRequest)
 		handleError(w, "Bad Request!", http.StatusBadRequest)
 		return
 	}
 	result, checkError := functions.HandelAsciiArt(text[0], banner[0])
 	if checkError {
-		// http.Error(w, "400 Bad Request!", http.StatusBadRequest)
 		handleError(w, "Bad Request!", http.StatusBadRequest)
 		return
 	}
 	execError := tmp.Execute(w, result)
 	if execError != nil {
-		// http.Error(w, "500 Internal Server Error!", http.StatusInternalServerError)
 		handleError(w, "Internal Server Error!", http.StatusInternalServerError)
 		return
 	}
@@ -100,9 +89,15 @@ func handleError(w http.ResponseWriter, errorText string, statusCode int) {
 
 	tmpl, err := template.ParseFiles("templates/error.html")
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		http.Error(w, "500 Internal Server Error!", http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(statusCode)
-	tmpl.Execute(w, myMap)
+	execError := tmpl.Execute(w, myMap)
+	if execError != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		http.Error(w, "500 Internal Server Error!", http.StatusInternalServerError)
+		return
+	}
 }
